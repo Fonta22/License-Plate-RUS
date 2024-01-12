2dd40a0702e063abe5a28fc7d189dd7b3090ebb6
Fonta22 <paufontasp@gmail.com>
2024-01-12 01:26:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Avtonomera Checker 🚨
+> All data was taken from [Wikipedia](https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Russia).
+
+This program identifies the region of a **Russian license plate** by it's **registration region code**.
+
+![nomera](./img/png/nomera_2.png)
+
+## How does it work
+The program performs a *dichotomous search* (fastest searching algorithm) through Wikipedia's data, which has been adapted for it by myself. Once the registration region code is found, shows the region associated with it.
+
+![Registration Codes Map](./img/png/registration_codes_map.png)
+
+## Demo
+Let's try the program on a real car, like for example this beautiful **Mercedes-Benz S63 AMG**.
+
+![S63 AMG](./img/demo/S63_AMG.jpg)
+> Picture by [@avtonomer44rus](https://www.instagram.com/avtonomer44rus/)
+
+The first step is to identify the **registration region code**. We can see it pretty clearly with the example we saw earlier.
+
+![Plate](./img/demo/plate_44_marked.png)
+
+As we can see, the registration region code is `44`. Let's try it on the program now.
+
+![Demo](./img/demo/demo_program.png)
+
+After entering the file name of the Wikipedia data (stored @ `./data/avtonomera.txt`), the program found the region of the code we were looking for.
+
+The code 44 ended to be from **Kostroma** (Кострома).
+
+![Kostroma](./img/demo/Kostroma.jpg)
+
+> *Thanks for reading)*
---
+++ src/Avtonomera.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int MAX_PLATES = 160;
+const int EXIT = -1;
+
+struct Plate {
+    int code;
+    string region;
+};
+
+typedef Plate Plates_vector[MAX_PLATES];
+
+struct Plates {
+    Plates_vector vec;
+    int n;
+};
+
+void openFile(ifstream& fin, string& filename) {
+    cout << "File name: ";
+    cin >> filename;
+    fin.open(filename.c_str());
+
+    while (not fin.is_open()) {
+        cout << "File not found. try again." << endl;
+        cout << "File name: ";
+        cin >> filename;
+        fin.open(filename.c_str());
+    }
+}
+
+void readPlate(ifstream& fin, Plate& p) {
+    fin >> p.code;
+    if (not fin.eof()) {
+        fin >> p.region;
+    }
+}
+
+void insert(Plates& plates, Plate& p) {
+    int i = plates.n - 1;
+    while (i >= 0 and p.code < plates.vec[i].code) {
+        plates.vec[i + 1] = plates.vec[i];
+        i--;
+    }
+    plates.vec[i + 1] = p;
+    plates.n++;
+}
+
+void readFile(ifstream& fin, Plates& plates) {
+    int i = 0;
+    Plate p;
+    readPlate(fin, p);
+    while (not fin.eof()) {
+        insert(plates, p);
+        i++;
+        readPlate(fin, p);
+    }
+    plates.n = i;
+    cout << "Data loaded." << endl;
+}
+
+void askCode(int& c) {
+    cout << "CODE: ";
+    cin >> c;
+}
+
+void searchCode(const Plates& plates, int code, bool& found, int& pos) {
+    int a = 0;
+    int b = plates.n - 1;
+    int m = 0;
+    found = false;
+    while (a <= b and not found) {
+        m = (a + b) / 2;
+        if (code == plates.vec[m].code) found = true;
+        else if (code < plates.vec[m].code) b = m - 1;
+        else a = m + 1;
+    }
+    if (found) pos = m;
+    else pos = a;
+}
+
+void printRegion(string region) {
+    for (char& c : region)
+        if (c == '_') cout << ' ';
+        else cout << c;
+    cout << endl;
+}
+
+int main() {
+    ifstream fin;
+    string filename;
+    openFile(fin, filename);
+
+    Plates plates;
+    readFile(fin, plates);
+
+    for (int i = 0; i < plates.n; i++) {}
+
+    cout << "[" << EXIT << " TO EXIT]" << endl;
+
+    int code;
+    askCode(code);
+    while (code != EXIT) {
+        bool found;
+        int pos;
+        searchCode(plates, code, found, pos);
+
+        if (not found) {
+            cout << "Registration code not found, please try again." << endl;
+        } else {
+            string region = plates.vec[pos].region;
+            printRegion(region);
+        }
+
+        askCode(code);
+    }
+
+    return 0;
+}
